8e160fa4aff028f25cbe4e2299a4ab97861b522c
Ákos Gángoly <gangoly96@gmail.com>
2020-10-10 08:21:40 +0000

    sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("./.."))
 
@@ -28,7 +29,7 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "autoapi.extension"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "autoapi.extension", "sphinx_rtd_theme"]
 # Document Python Code
 autoapi_type = 'python'
 autoapi_dirs = ['../fll']
