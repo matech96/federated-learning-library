936911047f3c7832cff9155c8c9838e4b4a17de8
Ákos Gángoly <gangoly96@gmail.com>
2021-02-16 18:09:49 +0100

    python interlink

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,11 @@ author = 'Ákos Gángoly'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
 autodoc_typehints = 'description'
+autoclass_content = 'both'
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
